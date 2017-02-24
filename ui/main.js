ec72ffe60f3c404d64cba5f7fd1796987e070fa4
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-24 19:15:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // move the image 
 
-var img = document.getElementById("img");
+var img = document.getElementById("nivas");
 
 img.onclick=function(){
     img.style.marginLeft="100px";
