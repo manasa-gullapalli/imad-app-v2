ccbb791a23fdb6e2ae1f4b7f3bf83462c3ecb195
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-24 19:19:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 
 var img = document.getElementById("nivas");
 
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft=5;
+    img.style.marginLeft=marginLeft+"px";
+}
+
 img.onclick=function(){
-    img.style.marginLeft="100px";
+
+    var interval=setInterval(moveRight,50);
 };
