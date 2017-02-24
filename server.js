ace011f7b655d38476cb87648cc2e39b131c54a2
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-24 20:48:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var counter=0;
 
 app.get('/counter',function (req,res){
  counter=counter+1;
- res.send(counter.toString());
+ res.send(counter,toString());
 });
 app.get('/nivas',function (req,res){
     
