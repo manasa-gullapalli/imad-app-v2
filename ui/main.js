84014b8d2316550bccc9b39d4cc6eeddded788fa
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-24 20:22:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,10 @@
-console.log('Loaded!');
-// move the image 
+var button = document.getElementById("counter");
+var counter=0;
 
-var img = document.getElementById("nivas");
-
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+"px";
-}
-
-img.onclick=function(){
-
-    var interval=setInterval(moveRight,50);
+button.onclick=function(){
+  
+  counter=counter+1;
+  
+  var span=document.getElementById("count");
+  span.innerHTMl=count,toString();
 };
