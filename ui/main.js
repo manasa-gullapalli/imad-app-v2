5c80013929c34672161ef23def672f4703285d4b
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-26 16:47:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,15 +3,15 @@ var button = document.getElementById('counter');
 
 button.onclick=function(){
   
-  var request = new XMLhttpRequest();
-  request.onreadystatechange = fuction(){
+  var request = new XMLhttpRequest() ;
+  request.onreadystatechange =fuction();{
     if(request.readyState == XMLHttpRequest.DONE){
         if(request.status == 200){
             var counter=request.responseTest;
             var span=document.getElementById('count');
             span.innerHTML= counter,Tostring() ;
-        }
-    }  
+                                  }
+                                                   }
   }
   
   requset.open('GET','http://manasa-gullapalli.imad.hasura-app.io/counter',true);
