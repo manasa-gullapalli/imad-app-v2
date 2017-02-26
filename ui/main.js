a5a51a8701c88635a9a1acc829778742d9e1debd
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-26 17:02:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ button.onclick=function(){
   var request = new XMLHttpRequest() ;
   request.onreadystatechange =function(){
     if(request.readyState == XMLHttpRequest.DONE){
-        if(request.status == 200){
+        if(request.status == 500){
             var counter=request.responseTest;
             var span=document.getElementById('count');
             span.innerHTML= counter;
