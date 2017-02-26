a2d74ed73aa644a308d0d8dd8993e0dc91faa77a
manasa-gullapalli <ee16b015@smail.iitm.ac.in>
2017-02-26 17:00:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onclick=function(){
         if(request.status == 200){
             var counter=request.responseTest;
             var span=document.getElementById('count');
-            span.innerHTML= counter,toString();
+            span.innerHTML= counter;
                                   }
                                                    }
   };
